ef87021a62126ae1509f8a441e9bb30c1e017218
Time <33477685+Lemon7ProPlus@users.noreply.github.com>
2021-07-09 21:42:11 +0800

    Update index.js

--- script/index.js
+++ script/index.js
@@ -20,22 +20,22 @@ function opensearchbarlist() {
   document.getElementById("searchbarlist").style.display="block";
 }
 function searchbar_google() {
-  document.getElementById("searchbaricon").src='/blob/main/img/google.png';
+  document.getElementById("searchbaricon").src='/Via/img/google.png';
   document.getElementById("searchbarlist").style.display="none";
   searchengiecurrent="google";
 }
 function searchbar_bing() {
-  document.getElementById("searchbaricon").src='/blob/main/img/bing.png';
+  document.getElementById("searchbaricon").src='/Via/img/bing.png';
   document.getElementById("searchbarlist").style.display="none";
   searchengiecurrent="bing";
 }
 function searchbar_baidu() {
-  document.getElementById("searchbaricon").src='/blob/main/img/baidu.png';
+  document.getElementById("searchbaricon").src='/Via/img/baidu.png';
   document.getElementById("searchbarlist").style.display="none";
   searchengiecurrent="baidu";
 }
 function searchbar_duckgo() {
-  document.getElementById("searchbaricon").src='/blob/main/img/duckgo.png';
+  document.getElementById("searchbaricon").src='/Via/img/duckgo.png';
   document.getElementById("searchbarlist").style.display="none";
   searchengiecurrent="duckgo";
 }
